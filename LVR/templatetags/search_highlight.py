57c94d2fe7759af449496afe33a1c2b3a7d64a7d
Jorge Falcón <jorgef_campos@outlook.com>
2020-07-31 22:35:56 -0500

    Integrated a custom filter for highlighting queries matches

---
+++ LVR/templatetags/search_highlight.py
@@ -0,0 +1,9 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(name='highlight')
+def highlight(text, search):
+    highlighted = text.replace(search, '<span class="highlight">{}</span>'.format(search))
+    return mark_safe(highlighted)
